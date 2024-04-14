45a80a5abae92a5e3623c2fa1ed1d16681df45b0
SevenArmy99 <proadrian1999@gmail.com>
2024-04-14 16:19:32 +0200

    Header empezado , Card recetas casi lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { CardsRecetasComponent } from './cards-recetas/cards-recetas.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent,CardsRecetasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/cards-recetas/cards-recetas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cards-recetas',
+  standalone: true,
+  imports: [],
+  templateUrl: './cards-recetas.component.html',
+  styleUrl: './cards-recetas.component.css'
+})
+export class CardsRecetasComponent {
+
+}
