129ee84e81ebefa7569379f96f50b2677d835f8c
SevenArmy99 <proadrian1999@gmail.com>
2024-05-19 17:26:41 +0200

    Crear Recetas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { RecipeCreateComponent } from './recipe-create/recipe-create.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Cuando la ruta es vacía, se carga el componente HomeComponent
-    { path: 'sign-in', component: SignInComponent }
+    { path: 'sign-in', component: SignInComponent },
+    { path: 'recipe-create', component: RecipeCreateComponent }
 ];
---
+++ src/app/recipe-create/recipe-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './recipe-create.component.html',
+  styleUrl: './recipe-create.component.css'
+})
+export class RecipeCreateComponent {
+
+    
+}
