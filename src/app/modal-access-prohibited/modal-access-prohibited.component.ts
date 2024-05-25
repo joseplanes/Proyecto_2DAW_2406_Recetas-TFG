bc6e9d933fbe171c2e3960b7d8908f597dbce30d
jorgetamaral <126577001+jorgetamaral@users.noreply.github.com>
2024-05-25 19:33:14 +0200

    modal de acceso prohibido creado

---
+++ src/app/modal-access-prohibited/modal-access-prohibited.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-modal-access-prohibited',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './modal-access-prohibited.component.html',
+  styleUrl: './modal-access-prohibited.component.css'
+})
+export class ModalAccessProhibitedComponent {
+
+}
--- src/app/user-view/user-view.component.ts
+++ src/app/user-view/user-view.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ModalAccessProhibitedComponent } from '../modal-access-prohibited/modal-access-prohibited.component';
 
 @Component({
   selector: 'app-user-view',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, ModalAccessProhibitedComponent],
   templateUrl: './user-view.component.html',
   styleUrl: './user-view.component.css'
 })
 export class UserViewComponent {
+  logged: boolean = false;
+  following: boolean = false;
 
+  constructor() { }
+
+  isLoged() {
+    return this.logged == true ? true : false;
+  }
+
+  isFollowing(){
+    return this.following == true ? 'Siguiendo' : 'Seguir';
+  }
 }
