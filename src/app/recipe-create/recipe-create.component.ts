ca4770c328ef1b257d058edcb00b03edafa1b2f6
SevenArmy99 <proadrian1999@gmail.com>
2024-05-31 16:46:55 +0200

    Echo ya la seleccion de los botones de la dificultad

--- src/app/recipe-create/recipe-create.component.ts
+++ src/app/recipe-create/recipe-create.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-recipe-create',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recipe-create.component.html',
   styleUrl: './recipe-create.component.css'
 })
 export class RecipeCreateComponent {
-
+  dificultad: string = '';
     
 }
