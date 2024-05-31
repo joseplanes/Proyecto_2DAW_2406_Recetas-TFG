c1abe7751250fd3fbbc487f6a5c1e272d5195534
jorgetamaral <126577001+jorgetamaral@users.noreply.github.com>
2024-05-31 17:52:07 +0200

    antes de preline

--- src/app/browse-recipes/browse-recipes.component.ts
+++ src/app/browse-recipes/browse-recipes.component.ts
@@ -2,11 +2,12 @@ import { Component, inject } from '@angular/core';
 import { CardsRecetasComponent } from '../cards-recetas/cards-recetas.component';
 import { RouterLink, RouterModule } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { UserViewRecipeComponent } from '../user-view/user-view-recipe/user-view-recipe.component';
 
 @Component({
   selector: 'app-browse-recipes',
   standalone: true,
-  imports: [CardsRecetasComponent ,RouterModule],
+  imports: [CardsRecetasComponent ,RouterModule, UserViewRecipeComponent],
   templateUrl: './browse-recipes.component.html',
   styleUrl: './browse-recipes.component.css'
 })
