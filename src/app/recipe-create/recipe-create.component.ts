8d109e4078d6eab3e8e1035cf484b179d1222c23
SevenArmy99 <proadrian1999@gmail.com>
2024-06-04 20:02:05 +0200

    Casi acabada create-recipe  a falta de la logica del post

--- src/app/recipe-create/recipe-create.component.ts
+++ src/app/recipe-create/recipe-create.component.ts
@@ -1,23 +1,45 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-recipe-create',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './recipe-create.component.html',
   styleUrl: './recipe-create.component.css'
 })
 export class RecipeCreateComponent {
   dificultad: string = '';
-  ingredientes: string[] = [''];
-  pasos: string[] = ['1.'];
+  ingredientes = [{ nombre: '', cantidad: '' }];
+  pasos: string[] = [''];
+
   addIngrediente() {
-    this.ingredientes.push('');
+    this.ingredientes.push({ nombre: '', cantidad: '' });
   }
 
+  updateIngrediente(i: number, prop: 'nombre' | 'cantidad', event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    if (target) {
+        this.ingredientes[i][prop] = target.value;
+    }
+  }
   addPasos(){
     this.pasos.push('');
+    console.log(this.pasos);
   }
+  updatePasos(i: number, event: Event) {
+    const target = event.target as HTMLInputElement | null;
+    if (target) {
+        this.pasos[i] = target.value;
+    }
+  }
+  trackByIndex(index: number): number {
+    return index;
+  }
+
 }
+
+
