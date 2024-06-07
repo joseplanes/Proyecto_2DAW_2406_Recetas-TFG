bfe0190bcf95277f33138eae187b8c3e647420a6
jorgetamaral <126577001+jorgetamaral@users.noreply.github.com>
2024-06-07 14:29:28 +0200

    page404 added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { RecipeCreateComponent } from './recipe-create/recipe-create.component';
 import { BrowseRecipesComponent } from './browse-recipes/browse-recipes.component';
@@ -7,6 +8,7 @@ import { PasswordRecoveryComponent } from './auth/password-recovery/password-rec
 import { UserViewComponent } from './user-view/user-view.component';
 import { RecipeComponent } from './recipe/recipe.component';
 import { CreateAccountComponent } from './auth/create-account/create-account.component';
+import { Page404Component } from './page404/page404.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, // Cuando la ruta es vacía, se carga el componente HomeComponent
@@ -17,5 +19,5 @@ export const routes: Routes = [
     { path: "sign-in/create-account", component: CreateAccountComponent},
     { path: 'user-view', component: UserViewComponent } ,
     { path: 'recipe', component: RecipeComponent } ,
-    { path: '**', redirectTo: '' } // Si la ruta no existe, redirijo a la raíz
+    { path: '**', component: Page404Component } // Si la ruta no existe, redirijo a page404
 ];
